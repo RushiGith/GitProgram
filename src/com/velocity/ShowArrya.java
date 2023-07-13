8ef8b298117b8a53b6d8fde76df9cc39655d0abd
rushi <rushikeshgomase2@gmail.com>
2023-07-13 11:19:36 +0530

    my second git change

---
+++ src/com/velocity/ShowArrya.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ShowArrya {
+
+	public static void main(String[] args) {
+	ArrayList al= new ArrayList();
+	al.add("Rushi");
+	al.add("Gomase");
+	al.add(775);
+	System.out.print(al);
+	}
+
+}
